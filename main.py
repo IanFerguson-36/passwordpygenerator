6f6b26dd079dbb45fa7c71819b86fadad10375e9
Ian F <ian.ferg36@gmail.com>
2023-03-24 13:28:38 -0600

    created user input prompts for password parameters

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ letters_list_upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
                       "J", "K", "L", "M", "N", "O", "P", "Q", "R",
                       "S", "T", "U", "V", "W", "X", "Y", "Z"]
 letters_list_lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
-                      "j", "k", "l", "m", "n", "o", "p", "q", "r"
+                      "j", "k", "l", "m", "n", "o", "p", "q", "r",
                       "s", "t", "u", "v", "w", "x", "y", "z"]
 special_char_list = ["!", "@", "#", "$", "%", "&"]
+selected_chars = []
 
 
-def passwordparams():
+def password_params():
     password_length = input("How many characters would you like your password to be? ")
-    password_length=int(password_length)
+    password_length = int(password_length)
 
-    password_nums= input("Would you like numbers in your password? y/n ")
+    password_nums = input("Would you like numbers in your password? y/n ")
+    if password_nums == "y":
+        selected_chars.extend(nums_list)
 
-passwordparams()
+    password_upper = input("Would you like uppercase letters in your password? y/n ")
+    if password_upper == "y":
+        selected_chars.extend(letters_list_upper)
+
+    password_lower = input("Would you like lowercase letters in your password? y/n ")
+    if password_lower == "y":
+        selected_chars.extend(letters_list_lower)
+
+    password_special = input("Would you like special characters in your password? y/n ")
+    if password_special == "y":
+        selected_chars.extend(special_char_list)
+    print(selected_chars)
+
+
+password_params()
